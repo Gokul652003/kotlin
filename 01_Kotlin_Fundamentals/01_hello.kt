5635f5bc3415cb51239cccd7fe5e2bb4d87872e5
Gokul K R <gokulkr652003@gmail.com>
2026-08-12 16:37:23 +0530

    01_hello.kt

---
+++ 01_Kotlin_Fundamentals/01_hello.kt
@@ -0,0 +1,10 @@
+// 01: Kotlin setup + first program
+// Run with: kotlinc 01_hello.kt -include-runtime -d 01.jar && java -jar 01.jar
+
+fun main() {
+    println("Hello, World!")
+
+    // Exercise: print your name and the result of some simple math
+    // TODO: println("My name is ___")
+    // TODO: println(___ + ___)
+}
