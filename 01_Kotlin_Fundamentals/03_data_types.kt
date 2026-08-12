4867650d3ed0adef389dcbc661a0079c2c04fe2c
Gokul K R <gokulkr652003@gmail.com>
2026-08-12 16:37:23 +0530

    03_data_types.kt

---
+++ 01_Kotlin_Fundamentals/03_data_types.kt
@@ -0,0 +1,19 @@
+// 03: Data types
+// Int, Long, Double, Float, Boolean, Char, String
+
+fun main() {
+    val count: Int = 42
+    val big: Long = 4_000_000_000L
+    val pi: Double = 3.14159
+    val approx: Float = 3.14f
+    val done: Boolean = true
+    val letter: Char = 'A'
+    val text: String = "hello"
+
+    // Exercise:
+    // 1. Store your age as Int, your height as Double.
+    // 2. Convert an Int to Long and to Double using .toLong() / .toDouble().
+    // TODO: val myAge: Int = ___
+    // TODO: val myHeight: Double = ___
+    // TODO: println(myAge.toLong())
+}
